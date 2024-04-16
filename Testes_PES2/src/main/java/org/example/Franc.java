024618b6d5a8b5cf9416359c8f011d4dddab4e92
Jared <gabrieljaredjared@gmail.com>
2024-04-16 11:49:33 -0300

    refatoracao + money

---
+++ Testes_PES2/src/main/java/org/example/Franc.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Franc extends Money{
+    Franc(int amount) {
+        this.amount= amount;
+    }
+    Franc times(int multiplier)  {
+        return new Franc(amount * multiplier);
+    }
+}
