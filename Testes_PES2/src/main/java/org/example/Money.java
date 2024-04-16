259d3cb7dbd0e3ad9bf1618d55b237308d88c298
Jared <gabrieljaredjared@gmail.com>
2024-04-16 12:15:44 -0300

    class money is now concrect

--- Testes_PES2/src/main/java/org/example/Money.java
+++ Testes_PES2/src/main/java/org/example/Money.java
@@ -1,19 +1,21 @@
 package org.example;
-abstract class Money  {
+class Money  {
     protected int amount;
-    abstract Money times(int multiplier);
     private String currency;
+    Money times(int multiplier) {
+        return new Money(amount * multiplier, currency);
+    }
     public boolean equals(Object object) {
         Money money = (Money) object;
-        return amount == money.amount && getClass().equals(money.getClass());
+        return amount == money.amount && currency().equals(money.currency());
     }
 
     static Money dollar(int amount)  {
-        return new Dollar(amount, "USD");
+        return new Money(amount, "USD");
     }
 
     static Money franc(int amount) {
-        return new Franc(amount, "CHF");
+        return new Money(amount, "CHF");
     }
 
     Money(int amount, String currency) {
